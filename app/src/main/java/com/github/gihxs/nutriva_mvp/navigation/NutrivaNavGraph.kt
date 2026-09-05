6b72b5e5962a34777ef34b12e048e5cfb817ad55
Giovana Santos <giovanasilva.office@gmail.com>
2026-09-04 23:51:06 -0300

    Add screen navigation

---
+++ app/src/main/java/com/github/gihxs/nutriva_mvp/navigation/NutrivaNavGraph.kt
@@ -0,0 +1,59 @@
+package com.github.gihxs.nutriva_mvp.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.github.gihxs.nutriva_mvp.ui.screens.faq.FaqScreen
+import com.github.gihxs.nutriva_mvp.ui.screens.home.HomeScreen
+import com.github.gihxs.nutriva_mvp.ui.screens.jornada.JornadaScreen
+import com.github.gihxs.nutriva_mvp.ui.screens.postos.PostoDetailScreen
+import com.github.gihxs.nutriva_mvp.ui.screens.postos.PostosListScreen
+import com.github.gihxs.nutriva_mvp.ui.screens.triagem.TriagemFormScreen
+import com.github.gihxs.nutriva_mvp.ui.screens.triagem.TriagemResultadoScreen
+
+@Composable
+fun NutrivaNavGraph() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = NutrivaScreen.Home.route) {
+
+        composable(NutrivaScreen.Home.route) {
+            HomeScreen(navController = navController)
+        }
+
+        composable(NutrivaScreen.Jornada.route) {
+            JornadaScreen(navController = navController)
+        }
+
+        composable(NutrivaScreen.PostosList.route) {
+            PostosListScreen(navController = navController)
+        }
+
+        composable(
+            route = NutrivaScreen.PostoDetail.route,
+            arguments = listOf(navArgument("postoId") { type = NavType.IntType })
+        ) { backStackEntry ->
+            val postoId = backStackEntry.arguments?.getInt("postoId") ?: -1
+            PostoDetailScreen(postoId = postoId, navController = navController)
+        }
+
+        composable(NutrivaScreen.TriagemForm.route) {
+            TriagemFormScreen(navController = navController)
+        }
+
+        composable(
+            route = NutrivaScreen.TriagemResultado.route,
+            arguments = listOf(navArgument("resultado") { type = NavType.StringType })
+        ) { backStackEntry ->
+            val resultado = backStackEntry.arguments?.getString("resultado") ?: "PENDENTE"
+            TriagemResultadoScreen(resultado = resultado, navController = navController)
+        }
+
+        composable(NutrivaScreen.Faq.route) {
+            FaqScreen(navController = navController)
+        }
+    }
+}
---
+++ app/src/main/java/com/github/gihxs/nutriva_mvp/navigation/NutrivaScreen.kt
@@ -0,0 +1,15 @@
+package com.github.gihxs.nutriva_mvp.navigation 
+
+sealed class NutrivaScreen(val route: String) {
+    object Home : NutrivaScreen("home")
+    object Jornada : NutrivaScreen("jornada")
+    object PostosList : NutrivaScreen("postos")
+    object PostoDetail : NutrivaScreen("postos/{postoId}") {
+        fun createRoute(postoId: Int) = "postos/$postoId"
+    }
+    object TriagemForm : NutrivaScreen("triagem")
+    object TriagemResultado : NutrivaScreen("triagem/resultado/{resultado}") {
+        fun createRoute(resultado: String) = "triagem/resultado/$resultado"
+    }
+    object Faq : NutrivaScreen("faq")
+}
