9cdea3acccc9a5a15103ed031e418ca03f472989
Giovana Santos <giovanasilva.office@gmail.com>
2026-09-04 23:24:06 -0300

    Configure main activity and app theme

--- app/src/main/java/com/github/gihxs/nutriva_mvp/ui/theme/Color.kt
+++ app/src/main/java/com/github/gihxs/nutriva_mvp/ui/theme/Color.kt
@@ -2,10 +2,13 @@ package com.github.gihxs.nutriva_mvp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val NutrivaDarkBlue = Color(0xFF253E7D)
+val NutrivaTeal = Color(0xFF14AEA6)
+val NutrivaTealLight = Color(0xFFE0F5F3)
+val NutrivaBackground = Color(0xFFF3F8FA)
+val NutrivaCardBackground = Color(0xFFFFFFFF)
+val NutrivaTextSecondary = Color(0xFF5C6B7A)
+val NutrivaOutline = Color(0xFFB9C9D0)
+val NutrivaOutlineVariant = Color(0xFFD9E5E9)
+val NutrivaError = Color(0xFFD9534F)
+val NutrivaSuccess = Color(0xFF2E9E6B)
---
+++ app/src/main/java/com/github/gihxs/nutriva_mvp/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package com.github.gihxs.nutriva_mvp.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val NutrivamvpShapes = Shapes(
+    small = RoundedCornerShape(10.dp),
+    medium = RoundedCornerShape(16.dp),
+    large = RoundedCornerShape(24.dp)
+)
